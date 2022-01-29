983bd99ae2a557deb919cfc4722cfef80373c7ab
stuart verschoyle <stuart_verschoyle@hotmail.co.uk>
2022-01-29 15:14:14 +0000

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,6 +181,24 @@
 				  }
 				  lastScrollTop = st;
 				}, 200);
-			  });		  
+			  });		
 
+			  var hashTagActive = "";
+			  $(".scroll").on("click touchstart" , function (event) {
+				  if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+					  event.preventDefault();
+					  //calculate destination place
+					  var dest = 0;
+					  if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+						  dest = $(document).height() - $(window).height();
+					  } else {
+						  dest = $(this.hash).offset().top;
+					  }
+					  //go to destination
+					  $('html,body').animate({
+						  scrollTop: dest
+					  }, 1000, 'swing');
+					  hashTagActive = this.hash;
+				  }
+			  });			  
 })(jQuery);
